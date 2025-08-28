9a4d175b2ea7332ed0e5bff9cb87a4b6b6b94b23
hepemo106-sys <hepemo106@gmail.com>
2025-08-28 13:08:34 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,9 +1,9 @@
-// Primero URL y KEY
+// URL y Key
 const SUPABASE_URL = "https://mrvjwygpojzljcnochnt.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ydmp3eWdwb2p6bGpjbm9jaG50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzNzEwMDEsImV4cCI6MjA3MTk0NzAwMX0.EFZmDNwtVvbJUd70IVZ8WervvroFHd0FsE47afqV-tE";
 
-// Crear cliente
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Crear cliente usando el objeto global `Supabase`
+const supabase = Supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 async function signup() {
     console.log("click en Crear Cuenta");
