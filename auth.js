6d88b4dee356670548740a2a0e8fdd5f653a1278
hepemo106-sys <hepemo106@gmail.com>
2025-08-28 13:03:46 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,9 +1,12 @@
+// Primero URL y KEY
 const SUPABASE_URL = "https://mrvjwygpojzljcnochnt.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ydmp3eWdwb2p6bGpjbm9jaG50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzNzEwMDEsImV4cCI6MjA3MTk0NzAwMX0.EFZmDNwtVvbJUd70IVZ8WervvroFHd0FsE47afqV-tE";
 
+// Crear cliente
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 async function signup() {
+    console.log("click en Crear Cuenta");
     const fullName = document.getElementById("fullName").value.trim();
     const dob = document.getElementById("dob").value;
     const email = document.getElementById("emailReg").value.trim();
@@ -18,6 +21,7 @@ async function signup() {
 }
 
 async function login() {
+    console.log("click en Iniciar Sesión");
     const email = document.getElementById("emailLogin").value.trim();
     const password = document.getElementById("passwordLogin").value.trim();
     if (!email || !password) return alert("Debes poner email y contraseña.");
@@ -26,17 +30,8 @@ async function login() {
     window.location.href = "dashboard.html";
 }
 
-async function logout() {
-    const { error } = await supabase.auth.signOut();
-    if (error) alert("Error al cerrar sesión: " + error.message);
-    else window.location.href = "index.html";
-}
-
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("btnSignup").addEventListener("click", signup);
     document.getElementById("btnLogin").addEventListener("click", login);
 });
-window.signup = signup;
-window.login = login;
-window.logout = logout;
 
